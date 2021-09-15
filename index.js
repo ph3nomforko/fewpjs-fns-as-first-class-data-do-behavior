3ab6cafd9a04d84592b94241424869573b70e3f6
Codi Kraus <codi.kraus@gmail.com>
2021-09-15 13:43:49 -0500

    commit tree a443ea03a29ed091f2686a696b5f5978d3a8d8e6

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  const splitTime = timeString.split(':');
+  const parsedTime = parseInt(splitTime);
+  if (parsedTime < 12) {
+    return "Good Morning"
+  }
+  else if (parsedTime > 12 && parsedTime < 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
